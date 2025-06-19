5553dd4dce9914328f3df70a9bb7a33b516d201b
Sudeep03 <106044394+Strangerme03@users.noreply.github.com>
2025-06-19 20:22:34 +0545

    updated fonts

--- weather/src/footer.jsx
+++ weather/src/footer.jsx
@@ -1,13 +1,33 @@
-function Footer(){
-    return(
-        <div className="footer">
-          <p>&copy; 2025 Sudeep Raj Karki. All Rights Reserved.</p>
-          <a href="https://twitter.com/Stranger_me03" target="_blank" class="text-dark me-3">Twitter</a>
-          <a href="https://www.facebook.com/karki.sudip.16/" target="_blank" class="text-dark me-3">Facebook</a>
-          <a href="https://www.instagram.com/stranger_me_03/" target="_blank" class="text-dark me-3">Instagram</a>
-          <a href="https://github.com/Strangerme03" target="_blank" class="text-dark me-3">GitHub</a>
-          <a href="https://www.linkedin.com/in/sudeep-raj-karki-15ab77238/" target="_blank" class="text-dark">LinkedIn</a>
-        </div>
-    )
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+  faGithub,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>&copy; 2025 Sudeep Raj Karki. All Rights Reserved.</p>
+      <a href="https://twitter.com/Stranger_me03" target="_blank" className="text-dark me-3" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faTwitter} /> 
+      </a>
+      <a href="https://www.facebook.com/karki.sudip.16/" target="_blank" className="text-dark me-3" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faFacebook} /> 
+      </a>
+      <a href="https://www.instagram.com/stranger_me_03/" target="_blank" className="text-dark me-3" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faInstagram} /> 
+      </a>
+      <a href="https://github.com/Strangerme03" target="_blank" className="text-dark me-3" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faGithub} /> 
+      </a>
+      <a href="https://www.linkedin.com/in/sudeep-raj-karki-15ab77238/" target="_blank" className="text-dark" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faLinkedin} /> 
+      </a>
+    </div>
+  );
 }
-export default Footer
+
+export default Footer;
